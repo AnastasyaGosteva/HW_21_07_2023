750751694b57c9e60c02382d054e39ebc6fad777
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-25 22:38:08 +0700

    Сделала task041

---
+++ task041/Program.cs
@@ -0,0 +1,15 @@
+﻿static int GetInput(String tex)
+{
+    Console.Write(tex);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+while (true)
+{
+    int number = GetInput("Введите любое целое число: ");
+    if (number > 0) count++;
+    Console.Clear();
+    Console.WriteLine($"Количество положительных чисел равно {count}.");
+    Console.WriteLine("Для завершения работы нажмите любой символ и 'Enter'.");
+}
